58f460deefef36087eacf76c3d946de009fb8027
Tyler Andersen <tyler@jtawebdesigns.com>
2014-04-15 14:07:43 -0600

    Adding actions

--- Libraries/Grid/Views/Cell.js
+++ Libraries/Grid/Views/Cell.js
@@ -1,4 +1,6 @@
 Ember.Fuel.Grid.View.Cell = Ember.Fuel.Grid.View.Base.extend({
   tagName: 'td', //td
-  classNames: ['fuel-grid-cell']
+  classNames: ['fuel-grid-cell'],
+  defaultTemplate: Ember.Handlebars.compile('{{output}}::Cell'),
+  templateName: 'fuel/grid/cell',
 });
--- ember-fuel.js
+++ ember-fuel.js
@@ -679,7 +679,9 @@ Ember.Fuel.Grid.View.Body = Ember.Fuel.Grid.View.CollectionBase.extend({
 
 Ember.Fuel.Grid.View.Cell = Ember.Fuel.Grid.View.Base.extend({
   tagName: 'td', //td
-  classNames: ['fuel-grid-cell']
+  classNames: ['fuel-grid-cell'],
+  defaultTemplate: Ember.Handlebars.compile('{{output}}::Cell'),
+  templateName: 'fuel/grid/cell',
 });
 
 })();
